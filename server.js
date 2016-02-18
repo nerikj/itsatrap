29e54d7205bd671708765e3eca3bfcaf4f598e0a
Erik Nordlund <erik.nordlund@mittmedia.se>
2016-02-18 19:06:17 +0100

    Create server

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import { Server } from "http";
+import socketio from "socket.io";
+
+const app = express();
+const http = Server(app);
+const io = socketio(http);
+
+const players = {};
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  players[socket] = {};
+  socket.emit('chat', 'Hi ' + socket.id);
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+http.listen(3000, () => {
+  console.log("Hi from lolball");
+});
--- src/app.js
+++ src/app.js
@@ -4,9 +4,17 @@
 import PIXI from 'pixi.js';
 import Game from './game.js';
 import Keyboard from './keyboard.js';
+import io from 'socket.io-client';
+
+const socket = io("http://localhost:3000");
+
+socket.on('connect', () => {
+  socket.on('chat', (msg) => {
+    console.log('message: ' + msg);
+  });
+});
 
 Keyboard.init();
 
 const g = new Game();
 g.start();
-
